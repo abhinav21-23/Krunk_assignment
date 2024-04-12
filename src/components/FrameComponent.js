1883fe067273f19ddb9664ba170697f54891ba67
abhinav21-23 <abhinavamity9410@gmail.com>
2024-04-12 09:22:07 +0530

    Krunk Assignment

---
+++ src/components/FrameComponent.js
@@ -0,0 +1,246 @@
+import "./FrameComponent.css";
+
+const FrameComponent = () => {
+  return (
+    <form className="frame-parent">
+      <div className="frame-group">
+        <div className="frame-container">
+          <button className="simple-chatbots-parent">
+            <img
+              className="simple-chatbots-icon"
+              alt=""
+              src="/simple-chatbots@2x.png"
+            />
+            <div className="name" />
+          </button>
+          <div className="xhesi-baze">
+            <div className="xhbazegmailcom">
+              <div className="email">
+                <div className="timpu">{`Timpu `}</div>
+              </div>
+              <img
+                className="password-input-container"
+                loading="lazy"
+                alt=""
+                src="/vector.svg"
+              />
+            </div>
+            <div className="chat-assistant-wrapper">
+              <div className="chat-assistant">Chat assistant</div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-wrapper">
+          <div className="new-moon-parent">
+            <div className="new-moon" />
+            <div className="online">Online</div>
+          </div>
+        </div>
+      </div>
+      <div className="new-moon1">
+        <div className="new-moon2">
+          <div className="new-moon3">
+            <div className="new-moon4">
+              <div className="hi-sam-i">
+                Hi Sam! I am your personal shopping assistant , how can i help
+                you today ?
+              </div>
+            </div>
+            <div className="new-moon5">
+              <div className="pm">4:45 PM</div>
+            </div>
+          </div>
+          <div className="chose-your-role">
+            <div className="i-am-looking-for-a-hand-bag-w-wrapper">
+              <div className="i-am-looking">
+                I am looking for a hand bag, with long strap .
+              </div>
+            </div>
+            <div className="pm-wrapper">
+              <div className="pm1">4:46 PM</div>
+            </div>
+          </div>
+          <div className="popular-tags-for-handbag-parent">
+            <div className="popular-tags-for">Popular tags for handbag</div>
+            <div className="phone-number">
+              <div className="component">
+                <button className="sign-in-message">
+                  <div className="already-have-an-account">
+                    <div className="clutch">Clutch</div>
+                  </div>
+                </button>
+                <button className="component-inner">
+                  <div className="fabric-lining-wrapper">
+                    <div className="fabric-lining">Fabric lining</div>
+                  </div>
+                </button>
+                <button className="component-child">
+                  <div className="baggit-wrapper">
+                    <div className="baggit">{`Baggit `}</div>
+                  </div>
+                </button>
+                <div className="frame-div">
+                  <input
+                    className="frame-child"
+                    placeholder="Multi Compartment"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="chevron-right-wrapper">
+                <img
+                  className="chevron-right-icon"
+                  loading="lazy"
+                  alt=""
+                  src="/chevronright.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="frame-parent1">
+            <div className="frame-parent2">
+              <div className="frame-parent3">
+                <div className="frame-parent4">
+                  <img
+                    className="frame-item"
+                    loading="lazy"
+                    alt=""
+                    src="/frame-5591@2x.png"
+                  />
+                  <div className="frame-parent5">
+                    <div className="frame-parent6">
+                      <div className="bags-on-timpu-wrapper">
+                        <div className="bags-on-timpu-container">
+                          <span className="bags-on">{`Bags on `}</span>
+                          <span className="timpu1">Timpu</span>
+                        </div>
+                      </div>
+                      <img
+                        className="vector-icon"
+                        loading="lazy"
+                        alt=""
+                        src="/vector-1.svg"
+                      />
+                    </div>
+                    <div className="products-parent">
+                      <div className="products">{`1123 products `}</div>
+                      <img
+                        className="chevron-right-icon1"
+                        loading="lazy"
+                        alt=""
+                        src="/chevronright-1.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="or-set-filter">
+                  Or set filter and help us choose the best bag for you.
+                </div>
+              </div>
+              <div className="pm-container">
+                <div className="pm2">4:48 PM</div>
+              </div>
+            </div>
+            <div className="frame-parent7">
+              <div className="select-filters-parent">
+                <div className="select-filters">Select filters</div>
+                <div className="filter-options">
+                  <img
+                    className="sliders-horizontal-icon"
+                    loading="lazy"
+                    alt=""
+                    src="/slidershorizontal.svg"
+                  />
+                  <div className="filter">Filter</div>
+                </div>
+              </div>
+              <div className="color-size-brand">
+                <div className="products-list">
+                  <div className="settings-menu">
+                    <div className="strap">Strap</div>
+                    <div className="message-input">-</div>
+                    <div className="long">Long</div>
+                  </div>
+                  <img
+                    className="check-square-2-icon"
+                    loading="lazy"
+                    alt=""
+                    src="/checksquare2.svg"
+                  />
+                </div>
+                <button className="colour-parent">
+                  <div className="colour">Colour</div>
+                  <img
+                    className="check-square-2-icon1"
+                    alt=""
+                    src="/checksquare2-1.svg"
+                  />
+                </button>
+                <div className="size-parent">
+                  <div className="size">Size</div>
+                  <img
+                    className="check-square-2-icon2"
+                    alt=""
+                    src="/checksquare2-1.svg"
+                  />
+                </div>
+                <div className="brand-parent">
+                  <div className="brand">Brand</div>
+                  <img
+                    className="check-square-2-icon3"
+                    alt=""
+                    src="/checksquare2-1.svg"
+                  />
+                </div>
+                <button className="material-parent">
+                  <div className="material">Material</div>
+                  <img
+                    className="check-square-2-icon4"
+                    alt=""
+                    src="/checksquare2-1.svg"
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="powered-by-logo-parent">
+        <div className="powered-by-logo">
+          <div className="type-your-message-parent">
+            <input
+              className="type-your-message"
+              placeholder="Type your message"
+              type="text"
+            />
+            <img className="paperclip-icon" alt="" src="/paperclip.svg" />
+          </div>
+          <div className="krunkai-logo">
+            <div className="powered-by-parent">
+              <div className="powered-by">{`Powered by `}</div>
+              <div className="krunkai">Krunk.ai</div>
+              <div className="logo-wrapper">
+                <img
+                  className="logo-icon"
+                  loading="lazy"
+                  alt=""
+                  src="/logo@2x.png"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="vector-wrapper">
+          <img
+            className="vector-icon1"
+            loading="lazy"
+            alt=""
+            src="/vector-2.svg"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default FrameComponent;
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,12 @@
+import FrameComponent from "../components/FrameComponent";
+import "./SignUpPage.css";
+
+const SignUpPage = () => {
+  return (
+    <div className="sign-up-page">
+      <FrameComponent />
+    </div>
+  );
+};
+
+export default SignUpPage;
